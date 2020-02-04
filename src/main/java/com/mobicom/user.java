65e418e0e63b752021f2daa5041aa98766e5aec0
stevenli91748 <stephanieliuebay@gmail.com>
2020-02-04 00:06:30 -0800

    this is demo 2

---
+++ src/main/java/com/mobicom/user.java
@@ -0,0 +1,16 @@
+package com.mobicom;
+
+/**
+ * @ Current Project : IntelliJ IDEA
+ * @ Current Package : com.mobicom
+ * @ Modify By       : gzpengli
+ * @ Date            : -2020/2/4-2020
+ * @ Time            : 0:04
+ **/
+public class user {
+
+    private String name;
+    private Integer id;
+
+
+}
